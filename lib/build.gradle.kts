469a4c67cb66d19fc07b792870531c9785c7d50a
Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
2022-06-30 10:12:15 +0200

    Setter opp Jitpack
    Co-authored-by: Tanet Trimas <tanet.trimas@nav.no>
    Co-authored-by: Christian Klem <christian.klem@nav.no>

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,12 +6,17 @@
  * User Manual available at https://docs.gradle.org/7.4.2/userguide/building_java_projects.html
  */
 
+group = "com.github.navikt"
+
+
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.5.31"
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
+
+    id("maven-publish")
 }
 
 repositories {
