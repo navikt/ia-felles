7386b903a5aa1bc05d552a5abfbc148e59cf20e2
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-11-02 13:13:05 +0100

    Typesikker bransjeid

---
+++ src/main/kotlin/ia/felles/definisjoner/bransjer/BransjeId.kt
@@ -0,0 +1,18 @@
+package ia.felles.definisjoner.bransjer
+
+sealed class BransjeId {
+    data class Næring(val næring: String) : BransjeId() {
+        init {
+            require(næring.matches(Regex("""\d{2}""")))
+        }
+    }
+
+    data class Næringskoder(val næringskoder: List<String>) : BransjeId() {
+        constructor(vararg næringskoder: String) : this(næringskoder.toList())
+
+        init {
+            require(næringskoder.isNotEmpty())
+            require(næringskoder.all { it.matches(Regex("""\d{5}""")) })
+        }
+    }
+}
