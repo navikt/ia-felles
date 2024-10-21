a39ca65acd9bb8e19f7cbdc4241269e4efaf72be
Sindre Larsen <silar.dev@proton.me>
2024-10-21 15:04:54 +0200

    Sletter gamle ubrukte felt

--- src/main/kotlin/ia/felles/integrasjoner/kafkameldinger/SpørreundersøkelseMelding.kt
+++ src/main/kotlin/ia/felles/integrasjoner/kafkameldinger/SpørreundersøkelseMelding.kt
@@ -1,14 +1,10 @@
 package ia.felles.integrasjoner.kafkameldinger
 
-import kotlinx.datetime.LocalDate
-
 interface SpørreundersøkelseMelding {
     val spørreundersøkelseId: String
-    val vertId: String? // TODO: Deprecate
     val orgnummer: String
     val virksomhetsNavn: String
     val status: SpørreundersøkelseStatus
-    val type: String? // TODO: Deprecate
-    val avslutningsdato: LocalDate? // TODO: Deprecate
+    val type: String
     val temaMedSpørsmålOgSvaralternativer: List<TemaMelding>
 }
--- src/main/kotlin/ia/felles/integrasjoner/kafkameldinger/TemaMelding.kt
+++ src/main/kotlin/ia/felles/integrasjoner/kafkameldinger/TemaMelding.kt
@@ -2,9 +2,6 @@ package ia.felles.integrasjoner.kafkameldinger
 
 interface TemaMelding {
     val temaId: Int
-    val navn: String? // TODO: Gjør ikke-nullable når gamle meldinger har gått ut på dato
-    val temanavn: Temanavn? // TODO Deprecate
-    val beskrivelse: String? // TODO Deprecate
-    val introtekst: String? // TODO Deprecate
+    val navn: String
     val spørsmålOgSvaralternativer: List<SpørsmålMelding>
 }
