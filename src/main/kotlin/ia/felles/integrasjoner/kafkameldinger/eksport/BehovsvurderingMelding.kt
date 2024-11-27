6e5bcab58d278aed586725be2700cb1d42cbf422
Sindre Larsen <silar.dev@proton.me>
2024-11-27 16:20:41 +0100

    Endrer type for behovsvurderingsstatus

--- src/main/kotlin/ia/felles/integrasjoner/kafkameldinger/eksport/BehovsvurderingMelding.kt
+++ src/main/kotlin/ia/felles/integrasjoner/kafkameldinger/eksport/BehovsvurderingMelding.kt
@@ -1,11 +1,12 @@
 package ia.felles.integrasjoner.kafkameldinger.eksport
 
+import ia.felles.integrasjoner.kafkameldinger.spørreundersøkelse.SpørreundersøkelseStatus
 import kotlinx.datetime.LocalDateTime
 
 interface BehovsvurderingMelding {
     val id: String
     val orgnr: String
-    val status: String
+    val status: SpørreundersøkelseStatus
     val samarbeidId: Int
     val saksnummer: String
     val opprettetAv: String
