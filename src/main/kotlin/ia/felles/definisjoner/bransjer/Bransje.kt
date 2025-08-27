a5e81c8a0c4867719db8f50cdfcd2e85d416dd6d
Sindre Larsen <silar.dev@proton.me>
2025-08-27 13:12:25 +0200

    Legger til bransjedefinsjoner for SN2025 næringskoder, inkluderer både SN2007 og SN2025

--- src/main/kotlin/ia/felles/definisjoner/bransjer/Bransje.kt
+++ src/main/kotlin/ia/felles/definisjoner/bransjer/Bransje.kt
@@ -1,5 +1,17 @@
 package ia.felles.definisjoner.bransjer
 
+// SN2007
+val barnehagerSN2007: List<String> = listOf("88911")
+val sykehusSN2007: List<String> = listOf("86101", "86102", "86104", "86105", "86106", "86107")
+val sykehjemSN2007: List<String> = listOf("87101", "87102")
+val transportSN2007: List<String> = listOf("49100", "49311", "49312", "49391", "49392")
+
+// SN2025
+val barnehagerSN2025: List<String> = listOf("85100")
+val sykehusSN2025: List<String> = listOf("86101", "86102", "86103", "86104")
+val sykehjemSN2025: List<String> = listOf("87101", "87102", "87103", "87104", "87105", "87106")
+val transportSN2025: List<String> = listOf("49110", "49120", "49310", "49320", "49330", "49340", "49390")
+
 /*
 
 Definisjonen av bransjer er hentet fra arbeidsmiljøportalen:
@@ -12,7 +24,7 @@ enum class Bransje(
 ) {
     BARNEHAGER(
         navn = "Barnehager",
-        bransjeId = BransjeId.Næringskoder("88911"),
+        bransjeId = BransjeId.Næringskoder((barnehagerSN2007 union barnehagerSN2025).toList()),
     ),
     NÆRINGSMIDDELINDUSTRI(
         navn = "Næringsmiddelindustri",
@@ -20,15 +32,15 @@ enum class Bransje(
     ),
     SYKEHUS(
         navn = "Sykehus",
-        bransjeId = BransjeId.Næringskoder("86101", "86102", "86104", "86105", "86106", "86107"),
+        bransjeId = BransjeId.Næringskoder((sykehusSN2007 union sykehusSN2025).toList()),
     ),
     SYKEHJEM(
         navn = "Sykehjem",
-        bransjeId = BransjeId.Næringskoder("87101", "87102"),
+        bransjeId = BransjeId.Næringskoder((sykehjemSN2007 union sykehjemSN2025).toList()),
     ),
     TRANSPORT(
         navn = "Transport",
-        bransjeId = BransjeId.Næringskoder("49100", "49311", "49312", "49391", "49392"),
+        bransjeId = BransjeId.Næringskoder((transportSN2007 union transportSN2025).toList()),
     ),
     BYGG(
         navn = "Bygg",
