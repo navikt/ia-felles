920d969308bba96e00824d0009ee8a1d6608a3c9
Sindre Larsen <silar.dev@proton.me>
2025-08-28 11:05:01 +0200

    Fjerner unødvendig cast til BransjeId

--- src/test/kotlin/ia/felles/definisjoner/bransjer/BransjeTest.kt
+++ src/test/kotlin/ia/felles/definisjoner/bransjer/BransjeTest.kt
@@ -13,8 +13,8 @@ internal class BransjeTest {
             assertTrue {
                 when (bransje.bransjeId) {
                     // Main og test er satt opp som separate moduler, så smart cast fungerer ikke
-                    is BransjeId.Næring -> (bransje.bransjeId as BransjeId.Næring).næring.matches(Regex("""\d{2}"""))
-                    is BransjeId.Næringskoder -> (bransje.bransjeId as BransjeId.Næringskoder).næringskoder.all {
+                    is BransjeId.Næring -> bransje.bransjeId.næring.matches(Regex("""\d{2}"""))
+                    is BransjeId.Næringskoder -> bransje.bransjeId.næringskoder.all {
                         it.matches(
                             Regex("""\d{5}"""),
                         )
